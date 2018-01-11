eb829c34a2b6fe0588117cf8175a8d1ae76e7688
Kundan Singh <kundan.singh979@gmail.com>
2018-01-11 22:27:54 +0530

    chaning and uncaught languages examples

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 var g = G$('John', 'Doe');
-console.log(g);
+
+// simple greeting
+g.greet();
+
+// chaining helps here!
+g.greet().greet(true);
+
+// changing language on the fly
+g.greet().setLang('es').greet(true);
+
+// unsupported language passed:
+g.greet().setLang('fr').greet(true);
