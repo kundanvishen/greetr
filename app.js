43c3161735004fa3b49a519d0d92aa7cd03fad79
Kundan Singh <kundan.singh979@gmail.com>
2018-01-11 22:29:51 +0530

    Remove examples

--- app.js
+++ app.js
@@ -1,13 +1,4 @@
 var g = G$('John', 'Doe');
 
-// simple greeting
-g.greet();
-
 // chaining helps here!
 g.greet().greet(true);
-
-// changing language on the fly
-g.greet().setLang('es').greet(true);
-
-// unsupported language passed:
-g.greet().setLang('fr').greet(true);
