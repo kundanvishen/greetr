c0dac4030e6205233ed8f6df3b620178ac626c2a
Kundan Singh <kundan.singh979@gmail.com>
2018-01-11 21:57:43 +0530

    Setting up our object and its prototype

--- Greetr.js
+++ Greetr.js
@@ -1,3 +1,21 @@
-(function(global, $){
+// Created by Kundan Singh <kundanvishen@gmail.com>
+(function (global, $) {
+
+  var Greetr = function(firstName, lastName, language){
+    return new Greetr.init(firstName, lastName, language);
+  }
+
+  Greetr.prototype = {};
+
+  Greetr.init = function(firstName, lastName, language){
+    var self = this;
+    self.firstName = firstName || '';
+    self.lastName = lastName || '';
+    self.language = language || 'en'; // english language by default
+  }
+
+  Greetr.init.prototype = Greetr.prototype;
+
+  global.Greetr = global.G$ = Greetr; // Assign to be used outside
 
 })(window, jQuery);
